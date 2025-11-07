7b067ea87afe8a71600543416a15f4e5b3405505
harsh <harshchaurasiya0007@gmail.com>
2025-11-07 18:42:53 +0530

    Rent-to-own

--- src/components/home/RentToOwn.tsx
+++ src/components/home/RentToOwn.tsx
@@ -41,7 +41,7 @@ const RentToOwn: React.FC = () => {
   ];
 
   return (
-    <div className="container px-4 sm:px-6 lg:px-30 py-8 mx-auto mb-10 sm:mb-12 lg:mb-16">
+    <div className="container px-4 sm:px-6 lg:px-30 py-8 mx-auto">
       {/* Main Heading */}
       <div className="text-left mb-6 sm:mb-12">
         <h2 className="text-2xl sm:text-3xl font-bold text-gray-900">
